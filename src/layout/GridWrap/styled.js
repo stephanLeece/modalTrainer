6a96bed723ae5f84dd5662c567e1b39098edcfea
Stephan Leece <stephan.leece@outlook.com>
2019-11-17 22:08:20 +0100

    fix imports in GridComponent

--- src/layout/GridWrap/styled.js
+++ src/layout/GridWrap/styled.js
@@ -1,17 +1,25 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-import { BREAK_POINTS } from '../../constants/BreakPoints';
+import { BREAK_POINTS } from 'constants/breakpoints';
 
 const Wrap = styled.div`
 	display: grid;
-	margin: 0 16px;
 	grid-template-columns: repeat(4, 1fr);
 	grid-column-gap: 16px;
 	@media ${BREAK_POINTS.laptopMin} {
-		margin: 0 32px;
 		grid-template-columns: repeat(12, 1fr);
 		grid-column-gap: 16px;
 	}
+	${({ isRoot }) =>
+		isRoot &&
+		css`
+			margin: 0 16px;
+			@media ${BREAK_POINTS.laptopMin} {
+				margin: 0 5%;
+				grid-template-columns: repeat(12, 1fr);
+				grid-column-gap: 16px;
+			}
+		`}
 `;
 
 export { Wrap };
