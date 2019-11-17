b713d44730a062cd2fe3415ddf5aca12e86e8b86
stephanLeece <stephan.leece@outlook.com>
2019-11-17 21:33:43 +0100

    fix lint errors

--- src/layout/GridWrap/GridWrap.jsx
+++ src/layout/GridWrap/GridWrap.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Wrap } from './styled';
 
-const GridWrap = ({ children, className }) => <Wrap className={className}>{children}</Wrap>;
+const GridWrap = ({ children, className }) => (
+	<Wrap className={className}>{children}</Wrap>
+);
 
 export default GridWrap;
