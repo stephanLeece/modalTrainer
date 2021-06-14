e3ec883d37d962269e7295db001028f59f8a8893
stephanLeeceGrover <stephanl@grover.com>
2021-06-14 22:01:03 +0200

    tone set up

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import * as Tone from 'tone';
 
 import { Wrap } from './styled';
 
-const Home = () => <Wrap>welcome to the home page!</Wrap>;
+const synth = new Tone.Synth().toDestination();
+
+const Home = () => {
+    const beep = () => {
+        synth.triggerAttackRelease('C4', '8n');
+    };
+    return (
+        <Wrap>
+            welcome to the home page!<button onClick={beep}>beep</button>
+        </Wrap>
+    );
+};
 
 export default Home;
